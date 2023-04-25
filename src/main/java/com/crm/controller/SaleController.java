0a0ff8c259c54c46e06a2c8798680364974b918a
ll <1907476390@qq.com>
2023-04-25 10:46:25 +0800

    删除

--- src/main/java/com/crm/controller/SaleController.java
+++ src/main/java/com/crm/controller/SaleController.java
@@ -7,6 +7,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,12 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@Controller("Sale")
 public class SaleController {
     @Autowired
     private SaleService service;
 
 
+    @RequestMapping("/query")
     public Result query(SalChance salChance, @RequestParam(defaultValue = "1") int pageNum,
                         @RequestParam(defaultValue = "4") int pageSize) {
         PageHelper.startPage(pageNum, pageSize);
